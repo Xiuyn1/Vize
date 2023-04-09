3a617da75b7b801f2d5779a0bc9f37b50a6e876d
Xiuyn1 <22120205031@ismu.edu.tr>
2023-04-09 22:54:41 +0300

    Initial commit

---
+++ src/main/java/org/example/MailGonderme.java
@@ -0,0 +1,8 @@
+package org.example;
+
+import java.io.*;
+import java.util.*;
+import javax.mail.*;
+import javax.mail.internet.*;
+import javax.mail.Authenticator;
+
---
+++ src/main/java/org/example/Menu.java
@@ -0,0 +1,6 @@
+package org.example;
+
+import java.io.*;
+import java.util.Scanner;
+
+
